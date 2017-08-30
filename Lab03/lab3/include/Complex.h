d17d5e533c5be63bc11976d0439389c40900625c
shadySource <shadySource2@gmail.com>
2017-08-30 13:31:43 -0400

    work

--- Lab03/lab3/include/Complex.h
+++ Lab03/lab3/include/Complex.h
@@ -1,65 +1,77 @@
 #include <cmath>
 
-namespace numbers{
+namespace numbers
+{
 
-    class Complex{
+    class Complex
+    {
         double r, i;//real and imaginary components of complex number
 
         public:
             //set the value of real and imaginary components of the complex number
-            void setVals(double R, double I){
+            void setVals(double R, double I)
+            {
                 r = R; i = I;
             }
 
             //returns real component
-            const double getReal(){
+            double getReal() const
+            {
                 return r;
             }
             
             //return simaginary component
-            const double getImag(){
+            double getImag() const
+            {
                 return i;
             }
 
             //returns magnitude of the complex number
-            const double getMag(){
+            double getMag() const
+            {
                 return sqrt(r*r + i*i);
             }
 
             //add 2 complex numbers
-            Complex operator+(Complex C){
+            inline Complex operator+(const Complex& C) const
+            {
                 return Complex(r + C.r, i + C.i);
             }
 
             //subtract two complex numbers
-            Complex operator-(Complex C){
+            inline Complex operator-(const Complex& C) const
+            {
                 return Complex(r - C.r, i - C.i);
             }
 
             //multiply two complex numbers
-            Complex operator*(Complex C){
+            inline Complex operator*(const Complex& C) const
+            {
                 return Complex(r*C.r - i*C.i, r*C.i + i*C.r);
             }
 
             //divide two complex numbers. computes recripical of second and multiplies
-            Complex operator/(Complex C){
+            inline Complex operator/(const Complex& C) const
+            {
                 double d(C.r*C.r + C.i*C.i); //denominator, time optimization
                 return *this * Complex(C.r/d, -1*C.i/d);
             }
 
-            friend std::ostream& operator<<(std::ostream&, Complex&); //write C to output stram out
+            friend inline std::ostream& operator<<(std::ostream&, const Complex&); //write C to output stram out
 
-            friend Complex operator/(double X, Complex C);// return recripical of C multiplied by scalar x
-        // Complex() : r(0), i(0) {}
+            friend inline Complex operator/(const double& X, const Complex C);// return recripical of C multiplied by scalar x
+
         Complex(double R=0, double I=0) : r(R), i(I) {}
     };
 
-    std::ostream& operator<<(std::ostream& out, Complex& C){
+    inline std::ostream& operator<<(std::ostream& out, const Complex& C)
+    {
         out << C.r << " + " << C.i << 'i';
         return out;
     }
 
-    Complex operator/(double X, Complex C){
+    inline Complex operator/(double X, Complex C)
+    {
         return Complex(X) / C;
     }
 
--- Lab03/lab3/src/main.cxx
+++ Lab03/lab3/src/main.cxx
@@ -1,14 +1,52 @@
 #include <iostream>
+#include <fstream>
 #include "Complex.h"
 
 using namespace std;
-// using namespace numbers;
+using namespace numbers;
+
+void TestComplex()
+{
+    Complex z1(1.4, 2.1), z2(-3.2, 4.6);
+
+    cout << z1*z2 + z1 / (z2 - z1) << endl;
+}
+
+// struct to initialize and store an array of complex nums
+struct ComplexArray
+{
+    Complex **array;
+    const int r, j; // number of reals and imaginaries in map
+    ComplexArray(double rStart, double rStop, double iStart, double iStop, int RealSamples=20, ImaginarySamples=24) //note: start/stop inclusive
+        : r(RealSamples), i(ImaginarySamples)
+    {
+        array = new *Complex[j];
+        for (int i(0); i < j; ++i)
+        {
+            array[i] = new Complex[r]
+        }
+        double Mr = (rStop - rStart) / (n - 1) //slope for linear mapping reals
+        double Mi = 
+        for (int i(0); i < n; ++i)
+        {
+            array[i].setVals() = m * i + start;
+        }
+    }
+    ~ComplexArray()
+    {
+        delete[] array;
+    }
+};
 
 int main(int argc, char **argv)
 {
-    numbers::Complex z1(1.4, 2.1), z2(-3.2, 4.6);
+    TestComplex();
 
-    numbers::Complex solution = z1*z2 + z1 / (z2 - z1);
+    ComplexArray c(-2, 18, 5);
 
-    cout << solution.getReal() << " + " << solution.getImag() << "i" << endl;
+    for (int i(0); i < c.n; ++i)
+    {
+        cout << c.array[i] << endl;
+    }
+
 }
