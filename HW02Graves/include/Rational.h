5917fecc3bf98a7813e2b774e8217a63b604e681
shadySource <shadySource2@gmail.com>
2017-08-30 20:05:31 -0400

    update rational

--- HW02Graves/include/Rational.h
+++ HW02Graves/include/Rational.h
@@ -1,20 +1,101 @@
 #include <cstdio>
 class Rational{
-    int n, d; //numerator, denominator
+
+    int numerator, denominator; //numerator, denominator
+
 public:
-    Rational operator+(Rational R){return Rational(n*R.d+d*R.n, d*R.d);}
-    Rational operator-(Rational R){return Rational(n*R.d-d*R.n, d*R.d);}
-    Rational operator*(Rational R){return Rational(n*R.n, d*R.d);}
-    Rational operator/(Rational R){return Rational(n*R.d, d*R.n);}
-    Rational operator+(int i){return Rational(n+d*i, d);}
-    Rational operator-(int i){return Rational(n-d*i, d);}
-    Rational operator*(int i){return Rational(n*i, d);}
-    Rational operator/(int i){return Rational(n, d*i);}
-    int getNumer(){return n;}
-    int getDenom(){return d;}
-    void print(){printf("%d/%d\n", n, d);}
-    int gcf(int x, int y, int z){ return y&&(z=x%y) ? gcf(y,z,0) : y;}
-    void r(){int f=gcf(n, d, 0); n/=f; d/=f;} //reduce
-    Rational() :n(0),d(1) {}
-    Rational(int N, int D) :n(N), d(D) {}
+    Rational operator+(Rational secondRational)
+    {
+        int tempNumerator(numerator * secondRational.denominator+denominator*secondRational.numerator);
+        int tempDenominator(denominator*secondRational.denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator-(Rational secondRational)
+    {
+        int tempNumerator(numerator * secondRational.denominator-denominator*secondRational.numerator);
+        int tempDenominator(denominator*secondRational.denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator*(Rational secondRational)
+    {
+        int tempNumerator(numerator * secondRational.numerator);
+        int tempDenominator(denominator*secondRational.denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator/(Rational secondRational)
+    {
+        int tempNumerator(numerator * secondRational.denominator);
+        int tempDenominator(denominator*secondRational.numerator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator+(int integer)
+    {
+        int tempNumerator(numerator + denominator * integer);
+        int tempDenominator(denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator-(int integer)
+    {
+        int tempNumerator(numerator - denominator * integer);
+        int tempDenominator(denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator*(int integer)
+    {
+        int tempNumerator(numerator * integer);
+        int tempDenominator(denominator);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    Rational operator/(int integer)
+    {
+        int tempNumerator(numerator);
+        int tempDenominator(denominator * integer);
+        return Rational(tempNumerator, tempDenominator);
+    }
+
+    int getNumer()
+    {
+        return numerator;
+    }
+
+    int getDenom()
+    {
+        return denominator;
+    }
+
+    void print()
+    {
+        printf("%d/%d\n", numerator, denominator);
+    }
+
+    int GreatestCommonFactor(int x, int y, int z)
+    {
+        z = x % y;
+        if (y && z)
+        {
+            return GreatestCommonFactor(y,z,0);
+        }
+        else
+        {
+            return y;
+        }
+        return y&&(z=x%y) ? GreatestCommonFactor(y,z,0) : y;
+    }
+
+    void Reduce() //reduce
+    {
+        int factor(GreatestCommonFactor(numerator, denominator, 0));
+        numerator/=factor;
+        denominator/=factor;
+    }
+
+    Rational() :numerator(0),denominator(1) {}
+    Rational(int Numerator, int Denominator) :numerator(Numerator), denominator(Denominator) {}
 };
